6e4ec158e4b771df6a3fc938d8ffe16ff15ff9b0
Gloria Friesen and Janek Brandt <janek@>
2017-03-20 15:27:17 -0700

    add spark and velocity front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,12 +8,19 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    Scrabble scrabble = new Scrabble();
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/CHANGEME.vtl");
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/result", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("score", scrabble.calculateScore(request.queryParams("word")));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
